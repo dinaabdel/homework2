5c9b4765dd87ea940cfbd7733a51eee9e2041823
Muhammad Ayesh <Muhammad.Ayesh@eng.hu.edu.jo>
2019-04-20 16:58:34 +0300

    Muhammad Ayesh Review

--- src/homwork2/Utility.java
+++ src/homwork2/Utility.java
@@ -3,7 +3,7 @@ package homwork2;
 import java.util.Scanner;
 
 public class Utility {
-    String  word;
+    String word;
     //geter&setter
 
     public String getWord() {
@@ -15,52 +15,55 @@ public class Utility {
     }
 
     //method
-    public int numthea(String word){
-        char [] SameWord= word.toCharArray();
-       // String arr=new String("the");
-       // String ar=new String("a" );
-       // String wor=new String("word");
-        int sizeofcahar=SameWord.length;
-        int the=0;
-        int count=0;
-        for(int i=0;i<sizeofcahar;i++){
-            if(SameWord[i]=='a'){
+    public int numthea(String word) {
+        char[] SameWord = word.toCharArray();
+        // String arr=new String("the");
+        // String ar=new String("a" );
+        // String wor=new String("word");
+        int sizeofcahar = SameWord.length;
+        int the = 0;
+        int count = 0;
+        for (int i = 0; i < sizeofcahar; i++) {
+            if (SameWord[i] == 'a') {
                 count++;
             }//lower case "a"
-            if(SameWord[i]=='A'){
+            if (SameWord[i] == 'A') {
                 count++;
             }//upper case "A"
-            if (SameWord[i]=='t'&&SameWord[i+1]=='h'&&SameWord[i+2]=='e')
-            {
+            if (SameWord[i] == 't' && SameWord[i + 1] == 'h' && SameWord[i + 2] == 'e') {
                 count++;
             }//lower case of "the"
-       if(SameWord[i]=='T'&&SameWord[i+1]=='H'&&SameWord[i+2]=='E'){
-           count++;
-        }// the upper case "THE"
+            if (SameWord[i] == 'T' && SameWord[i + 1] == 'H' && SameWord[i + 2] == 'E') {
+                count++;
+            }// the upper case "THE"
         }
         return count;
-}
-public void  plaindrom(String word){
-        char [] love=word.toCharArray();
-        int count=0;
-        int count1=0;
-        int sizeLove=love.length;
-        for(int i=0;i<sizeLove;i++){
-            if (love[i]!=' '){
+    }
+
+    /**
+     * Palindrome means, if you reversed the word, the word will not be changed, for Example word 'BOB'
+     * I don't care about the business logic too much but I wanted you to know it, if you face this term one day.
+     */
+    public void plaindrom(String word) {
+        char[] love = word.toCharArray();
+        int count = 0;
+        int count1 = 0;
+        int sizeLove = love.length;
+        for (int i = 0; i < sizeLove; i++) {
+            if (love[i] != ' ') {
                 count1++;
-            }
-            else{
+            } else {
                 count++;
             }
         }
-    System.out.println("the number of ' '"+count+"there is not ' ' space in word number char"+count1);
-}
+        System.out.println("the number of ' '" + count + "there is not ' ' space in word number char" + count1);
+    }
 
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        Utility obj=new Utility();
+        Scanner in = new Scanner(System.in);
+        Utility obj = new Utility();
         System.out.println("enter the word ");
-        String d=in.nextLine();
+        String d = in.nextLine();
         obj.setWord(d);
         obj.getWord();
         System.out.println(obj.getWord());
